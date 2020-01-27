757483483b980992ad2b8112f4692b64381d929a
Jeffrey Brown <brownjs03@gmail.com>
2020-01-27 03:11:44 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,9 +1,8 @@
 class CreateActors < ActiveRecord::Migration[5.2] 
   def change
     create_table :actors do |t|
-      t.string :name 
-      t.integer :actor_id 
-      t.integer :show_id 
+      t.string :first_name 
+      t.string :last_name 
     end 
   end
 end 
